3e58585781bc7230903c32f5fecb1a1e52d5202c
anshuman0se <147651313+anshuman0se@users.noreply.github.com>
2025-04-11 17:48:02 -0400

    Update Function.cs

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -5,7 +5,6 @@ using System.Text;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 
-
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace EsepWebhook;
@@ -14,36 +13,24 @@ public class Function
 {
     public string FunctionHandler(object input, ILambdaContext context)
     {
-        context.Logger.LogInformation($"Received input: {input}");
-
         try
         {
-
             dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
             string issueUrl = json.issue.html_url;
-            context.Logger.LogInformation($"Issue URL: {issueUrl}");
-            string slackMessage = JsonConvert.SerializeObject(new
-            {
-                text = $"New GitHub Issue Created: {issueUrl}"
-            });
-            string slackUrl = Environment.GetEnvironmentVariable("SLACK_URL");
-            if (string.IsNullOrEmpty(slackUrl))
-            {
-                return "SLACK_URL environment variable is missing.";
-            }
+            string slackMessage = JsonConvert.SerializeObject(new { text = $"New GitHub Issue Created: {issueUrl}" });
+
             var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Post, slackUrl)
+            var request = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
             {
                 Content = new StringContent(slackMessage, Encoding.UTF8, "application/json")
             };
+
             var response = client.Send(request);
             using var reader = new StreamReader(response.Content.ReadAsStream());
-            string slackResponse = reader.ReadToEnd();
-            return $"Slack response: {slackResponse}";
+            return reader.ReadToEnd();
         }
         catch (Exception ex)
         {
-            context.Logger.LogError($"Exception: {ex.Message}");
             return $"Error: {ex.Message}";
         }
     }
